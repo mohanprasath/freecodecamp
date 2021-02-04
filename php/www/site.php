ad1e0f826096be0346129d810e1e0f156d7a07ca
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2021-02-04 12:08:27 +0200

    PHP Program #3 Variable Types

--- php/www/site.php
+++ php/www/site.php
@@ -7,13 +7,25 @@
 </head>
 <body>
 <?php 
- echo "<h3>Example 1:</h3>";
+  
+  echo "<h3>Program 1:</h3>";
   echo("Yay!");
   echo "<hr>";
-  echo "<h3>Example 2:</h3>";
-  $year = 2021;
-  $month = "February";
+
+  echo "<h3>Program 2:</h3>";
+  $year = 2021; # number variable
+  $month = "February"; # string variable
   echo "The current month and year(probably) are $month and $year";
+  echo "<hr>";
+
+  # program #3
+  echo "<h3>Program 3:</h3>";
+  $number = 2021;
+  $string = "February";
+  $decimal = 2021.0;
+  $isLeapYear = false;
+  echo "$number $string $decimal";
+  echo $isLeapYear ? 'true':'false';
   echo "<hr>";
  ?>
 
